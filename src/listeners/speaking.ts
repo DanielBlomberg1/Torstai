d871d472407f05071e3cb953fbc725a909bbc511
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-10-24 18:10:39 +0300

    Mäkkäri fixed and glizzy added

--- src/listeners/speaking.ts
+++ src/listeners/speaking.ts
@@ -3,7 +3,7 @@ import { Client, Guild, TextChannel } from "discord.js";
 import { PlaySoundEffect } from "../audio/SoundEffectPlayer";
 import { Print } from "../utils/Print";
 
-const audioclips = ["pallit", "väärä savu", "among us", "drift", "imen sun", "kakka", "skype", "windows", "hyvä fiilis", "McDonald's"];
+const audioclips = ["pallit", "väärä savu", "among us", "drift", "imen sun", "kakka", "skype", "windows", "hyvä fiilis", "Mäkkäri", "glizzy"];
 
 export const getPrefix = (g: Guild) => {
   return (global.mainTextChannels.get(g.id)?.commandPrefix as string) || ":D";
