5a8cc60aef58bcc597197432d0933752c1c0cfc1
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-12-21 18:30:27 +0200

    add default role to users schema

--- src/Database/schemas/usersmodel.ts
+++ src/Database/schemas/usersmodel.ts
@@ -3,6 +3,7 @@ import { Schema, model } from "mongoose";
 const users = new Schema({
   userId: String,
   username: String,
+  role: {type: String, default: "karhu"},
 });
 
 const usersmodel = model("Users", users, "users");
